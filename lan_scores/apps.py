53fd267bcf81076ddadb96ccbeb5b0697212ae69
pvauq <pietu.nospam@gmail.com>
2025-04-29 21:33:04 +0300

    migraatiotat

--- lan_scores/apps.py
+++ lan_scores/apps.py
@@ -5,6 +5,3 @@ class LanScoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lan_scores'
 
-    def ready(self):
-        from django.contrib.auth.models import Group
-        Group.objects.get_or_create(name='event_admin')
